7cf71774a16ebb7bf708d85cd289ce250c003826
christianlsb <chrislsb09@gmail.com>
2023-02-28 20:09:09 -0300

    feat: Created CategoryController

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -1,3 +1,29 @@
-import * as yup from 'yup';
+import * as yup from "yup";
 
 import Category from "../../app/models/Category";
+
+class CategoryController {
+  async store(request, response) {
+    const schema = yup.object().shape({
+      name: yup.string().required(),
+    });
+
+    try {
+      await schema.validateSync(request.body, { abortEarly: false });
+    } catch (err) {
+      return response.status(400).json({ error: err.errors });
+    }
+    const { name } = request.body;
+
+    const category = await Category.create({
+      name,
+    });
+    return response.json(category);
+  }
+  async index(request, response) {
+    const categories = await Category.findAll();
+    return response.json(categories);
+  }
+}
+
+export default new CategoryController();
